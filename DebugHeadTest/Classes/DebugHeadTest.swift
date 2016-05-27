c6a6f572ab67f53966e347f195650e685116970f
Koji Murata <koji.murata@dena.com>
2016-05-28 03:40:18 +0900

    publsh

--- DebugHeadTest/Classes/DebugHeadTest.swift
+++ DebugHeadTest/Classes/DebugHeadTest.swift
@@ -1,10 +1,10 @@
 import DebugHead
 
-class DebugHeadTest: DebugMenu {
-  static let debugMenuTitle = "exit"
-  static let debugMenuDangerLevel = DebugMenuDangerLevel.Extreme
-  static let debugMenuAccessoryType = UITableViewCellAccessoryType.None
-  static func debugMenuSelected(debugMenuTableViewController: UITableViewController) -> UIViewController? {
+public class DebugHeadTest: DebugMenu {
+  public static let debugMenuTitle = "exit"
+  public static let debugMenuDangerLevel = DebugMenuDangerLevel.Extreme
+  public static let debugMenuAccessoryType = UITableViewCellAccessoryType.None
+  public static func debugMenuSelected(debugMenuTableViewController: UITableViewController) -> UIViewController? {
     let alert = UIAlertController(title: "Exit", message: nil, preferredStyle: .Alert)
     alert.addAction(UIAlertAction(title: "ok", style: .Default, handler: { _ in
       exit(1)
